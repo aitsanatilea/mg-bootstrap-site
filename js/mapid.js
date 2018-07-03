cd5668c22efce292fa8ed4eec0acbabc2634df53
alieksia <a.aleksieienko@gmail.com>
2018-07-03 15:34:23 +0200

    Added product description, about section description and fixed scroll for map

--- js/mapid.js
+++ js/mapid.js
@@ -1,6 +1,11 @@
 "use strict";
 
-var mymap = L.map('mapid').setView([50.07112,14.40395],16);
+var mymap = L.map('mapid', {
+    center: [50.07112,14.40395],
+    zoom: 16,
+    scrollWheelZoom: false
+});
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 20,
